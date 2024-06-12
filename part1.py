2adb7a4bb52e005f41b051577f9db2fa2259cf7c
ANVod <157865931+ANVod@users.noreply.github.com>
2024-06-12 23:30:20 +0300

    add DZ  AZ02

--- part1.py
+++ part1.py
@@ -1,11 +1,20 @@
 import pandas as pd
 
+data = {
+                'name': ['Alice','Bob', 'Charlie', 'David', 'Eve'],
+                'gender': ['female', 'male', 'male', 'male', 'female'],
+                'department': ['HR', 'Engineering', 'Marketing', 'Engineering', 'HR']
+}
+df = pd.DataFrame(data)
 
-df = pd.read_csv('dz.csv')
+df['gender'] = df['gender'].astype('category')
+df['department'] = df['department'].astype('category')
 
+print(df['gender'].cat.categories)
+print(df['department'].cat.categories)
+print(df['gender'].cat.codes)
 
-average_salary_by_city = df.groupby('City')['Salary'].mean()
+df['department'] = df['department'].cat.remove_categories(['HR'])
+print(df['department'].cat.categories)
 
-# Вывод результата
-print("Средняя зарплата по городам:")
-print(average_salary_by_city)
+print(df)
